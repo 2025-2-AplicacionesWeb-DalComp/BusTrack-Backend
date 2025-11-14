99d68e464a17a6d648dc24cf86adc54ee104ab19
JoaquinC <u20201f788@upc.edu.pe>
2025-11-13 21:40:06 -0500

    feat: Create backend project

---
+++ BusTrack-Center-API/BusTrack.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,26 @@
+using BusTrack_center_API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusTrack_center_API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Create all entities configurations
+        
+        //Publishing Context
+        
+
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
