38b5211f1eb4454194c7beb3ab5dd536a1440947
AndS56 <andree567890@gmail.com>
2025-11-16 18:23:15 -0500

    Update GetProfileByIdQuery.cs

--- BusTrack-Center-API/BusTrack.API/Profile/Domain/Model/Query/GetProfileByIdQuery.cs
+++ BusTrack-Center-API/BusTrack.API/Profile/Domain/Model/Query/GetProfileByIdQuery.cs
@@ -1,3 +1,3 @@
-namespace ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Query;
+namespace BusTrack.API.Profiles.Domain.Model.Query;// si no funciona usen al inicio using BusTrack_center_API
 
 public record GetProfileByIdQuery(int ProfileId);
