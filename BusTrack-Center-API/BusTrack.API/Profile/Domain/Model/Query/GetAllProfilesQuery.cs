8d25302ff6cc93c7d1bc1642d699596100a6f719
AndS56 <andree567890@gmail.com>
2025-11-16 18:07:38 -0500

    Update GetAllProfilesQuery.cs

--- BusTrack-Center-API/BusTrack.API/Profile/Domain/Model/Query/GetAllProfilesQuery.cs
+++ BusTrack-Center-API/BusTrack.API/Profile/Domain/Model/Query/GetAllProfilesQuery.cs
@@ -1,4 +1,5 @@
-namespace ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Query;
+namespace BusTrack.API.Profiles.Domain.Model.Query; // si no funciona usen al inicio using BusTrack_center_API
+
 /// <summary>
 ///     Get All Profiles Query
 /// </summary>
